39ff6215fac22d5c0cb636236bc9ffe9aa58b234
asobhi <arman_sobhi@yahoo.com>
2018-05-01 18:08:05 -0500

    Implemented phases, player cannot play an action after buying a card

--- Game.py
+++ Game.py
@@ -8,6 +8,7 @@ class Game():
         for player_id in range(number_of_players):
             self.players.append(Player.Player(name='test',id=player_id,game=self))
         self.current_player = 0
+        self.phase = 'action'
 
     def set_up(self):
         for player in self.players:
@@ -18,11 +19,12 @@ class Game():
 
 
     def play_turn(self):
+        self.phase = 'action'
         player = self.players[self.current_player]
         print("player {}'s turn".format(self.current_player))
         action = "start"
         while action != "end":
-            self.prompt()
+            self.prompt(phase=self.phase)
             action = input("choose an action\n").lower()
             os.system('cls' if os.name == 'nt' else 'clear')
             if action == 'stats':
@@ -31,14 +33,16 @@ class Game():
                 player.show_hand()
             elif action == 'supply':
                 self.supply.show_supply()
-            elif action == 'play':
+            elif action == 'play' and self.phase == 'action':
                 player.choose_and_play_action()
             elif action == 'buy':
+                self.phase = 'buy'
                 if player.buy_card():
                     print("Card sucessfully purhcased")
                 else:
                     print("Purchased canceled or player out of buys")
             elif action == "end":
+                self.phase = 'clean_up'
                 player.clean_up()
                 self.current_player = (self.current_player + 1) % len(self.players)
             elif action == 'exit':
@@ -62,14 +66,15 @@ class Game():
     def opponents_of(self,player):
         return [opponent for opponent in self.players if opponent != player]
 
-    def prompt(self):
+    def prompt(self,phase):
         print('\n')
         print("Enter 'hand' to view your hand")
         print("Enter 'stats' to view your current stats")
         print("Enter 'supply' to view the supply")
         print("Enter 'examine' to examine a card from the supply")
-        print("Enter 'play' to play a card")
-        print("Enter 'buy' to purhcase a card'")
+        if phase == 'action':
+            print("Enter 'play' to play a card")
+        print("Enter 'buy to purhcase a card")
         print("Enter 'end' to end your turn")
         print("Enter 'exit' to exit this game")
         print('\n')
